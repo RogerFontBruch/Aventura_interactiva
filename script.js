32ee8adecd63fc406f87d962109a9c9c1aa03c9e
RogerFontBruch <39918412+RogerFontBruch@users.noreply.github.com>
2018-06-13 15:39:17 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,151 @@
+
+function passaPaginaMar(){
+    var paginaMar=document.getElementById('pagina2');
+    paginaMar.style.visibility='hidden';
+    var paginaZonaMar=document.getElementById('pagina3');
+    paginaZonaMar.style.visibility='visible';
+}
+
+function passaPaginaRiu(){
+    var paginaRiu=document.getElementById('pagina2');
+    paginaRiu.style.visibility='hidden';
+    var paginaZonaRiu=document.getElementById('pagina4');
+    paginaZonaRiu.style.visibility='visible';
+    
+    
+}
+
+function passaPaginaMarPunt(){
+    var paginaZonaMar=document.getElementById('pagina3');
+    paginaZonaMar.style.visibility='hidden';
+    var marZona1=document.getElementById('pagina5');
+    marZona1.style.visibility='visible';
+}
+function passaPaginaMarPont(){
+    var paginaZonaMar=document.getElementById('pagina3');
+    paginaZonaMar.style.visibility='hidden';
+    var marZona2=document.getElementById('pagina6');
+    marZona2.style.visibility='visible';
+}
+
+function passaPaginaRiuPunt(){
+    var paginaZonaRiu=document.getElementById('pagina4');
+    paginaZonaRiu.style.visibility='hidden';
+    var riuZona1=document.getElementById('pagina7');
+    riuZona1.style.visibility='visible';
+}
+function passaPaginaRiuPont(){
+    var paginaZonaRiu=document.getElementById('pagina4');
+    paginaZonaRiu.style.visibility='hidden';
+    var riuZona2=document.getElementById('pagina8');
+    riuZona2.style.visibility='visible';
+}
+
+
+
+
+function passaPagina2minMar(){
+    var marZona1=document.getElementById('pagina5');
+    marZona1.style.visibility='hidden';
+    var mar2Min=document.getElementById('minmar');
+    mar2Min.style.visibility='visible';
+    
+}
+
+function passaPagina2minMar2(){
+    var marZona2=document.getElementById('pagina6');
+    marZona2.style.visibility='hidden';
+    var mar2Min2=document.getElementById('minmar2');
+    mar2Min2.style.visibility='visible';
+    
+    
+}
+
+function passaPagina2minRiu(){
+    var riuZona1=document.getElementById('pagina7');
+    riuZona1.style.visibility='hidden';
+    var riu2Min=document.getElementById('minriu');
+    riu2Min.style.visibility='visible';
+    
+}
+
+function passaPagina2minRiu2(){
+    var riuZona2=document.getElementById('pagina8');
+    riuZona2.style.visibility='hidden';
+    var riu2Min2=document.getElementById('minriu2');
+    riu2Min2.style.visibility='visible';
+    
+}
+function passaPagina30minMar(){
+    var marZona1=document.getElementById('pagina5');
+    marZona1.style.visibility='hidden';
+    var mar30Min=document.getElementById('minsmar');
+    mar30Min.style.visibility='visible';
+    
+    
+}
+
+function passaPagina30minMar2(){
+    var marZona2=document.getElementById('pagina6');
+    marZona2.style.visibility='hidden';
+    var mar30Min2=document.getElementById('minsmar2');
+    mar30Min2.style.visibility='visible';
+    
+    
+}
+
+function passaPagina30minRiu(){
+    var riuZona1=document.getElementById('pagina7');
+    riuZona1.style.visibility='hidden';
+    var riu30Min=document.getElementById('minsriu');
+    riu30Min.style.visibility='visible';
+    
+    
+}
+
+function passaPagina30minRiu2(){
+    var riuZona2=document.getElementById('pagina8');
+    riuZona2.style.visibility='hidden';
+    var riu30Min2=document.getElementById('minsriu2');
+    riu30Min2.style.visibility='visible';
+    
+    
+}
+function passaPagina2hMar(){
+    var marZona1=document.getElementById('pagina5');
+    marZona1.style.visibility='hidden';
+    var mar2H=document.getElementById('hmar');
+    mar2H.style.visibility='visible';
+    
+    
+}
+
+function passaPagina2hMar2(){
+    var marZona2=document.getElementById('pagina6');
+    marZona2.style.visibility='hidden';
+    var mar2H2=document.getElementById('hmar2');
+    mar2H2.style.visibility='visible';
+    
+    
+}
+
+function passaPagina2hRiu(){
+    var riuZona1=document.getElementById('pagina7');
+    riuZona1.style.visibility='hidden';
+    var riu2H=document.getElementById('hriu');
+    riu2H.style.visibility='visible';
+    
+    
+}
+
+function passaPagina2hRiu2(){
+    var riuZona2=document.getElementById('pagina8');
+    riuZona2.style.visibility='hidden';
+    var riu2H2=document.getElementById('hriu2');
+    riu2H2.style.visibility='visible';
+    
+    
+}
+
+
+
